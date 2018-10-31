e857b62fec99040f021f29d4938b43d67ac4e9ad
Loic Coenen <ijwtcymb@gmail.com>
2018-10-31 20:39:49 +0000

    Issue 42, test A: included a picture importer.
    This commit have not be tested yet!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,6 +42,9 @@ import { MessageProvider } from '../providers/message/message';
 
 import { MomentModule } from 'ngx-moment';
 
+import { ImagePicker } from '@ionic-native/image-picker';
+import { Base64 } from '@ionic-native/base64';
+
 @NgModule({
 	declarations: [
 		MyApp,
@@ -112,7 +115,9 @@ import { MomentModule } from 'ngx-moment';
 			useClass: ApiKeyInterceptor, 
 			multi: true 
 		},
-		MessageProvider
+		MessageProvider,
+		ImagePicker,
+		Base64
 	]
 })
 export class AppModule {}
